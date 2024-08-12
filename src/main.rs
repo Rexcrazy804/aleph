9193bde01cf2276acbecba830f57372288f968bb
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-08-12 22:50:33 +0530

    added reqwest crate, some streamlining to onboard Sanoy

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+//use std::fs::File;
+//use std::io;
 use std::process::Command;
 
 fn main() {
-    //println!("Hello, Za WARUDO!");
-    //println!("I am totally running on {}", std::env::consts::OS);
+    println!("Hello, Za WARUDO!");
+    println!("I am totally running on {}", std::env::consts::OS);
 
-    let output = Command::new("pwsh")
-        .args(["-c", "echo 'Hello World'"])
+    let output = Command::new("cmd")
+        .args(["/C", "echo 'Hello World'"])
         .output()
         .expect("Failed to execute command");
 
@@ -15,4 +17,14 @@ fn main() {
     };
 
     println!("{text}");
+
+    //let response = reqwest::blocking::get(
+    //    "https://github.com/Clozure/ccl/releases/download/v1.12.2/ccl-1.12.2-windowsx86.zip",
+    //)
+    //.expect("failed to fetch url");
+    //
+    //let body = response.text().expect("No data");
+    //
+    //let mut out = File::create("rustup-init.sh").expect("failed to create file");
+    //io::copy(&mut body.as_bytes(), &mut out).expect("failed to copy content");
 }
