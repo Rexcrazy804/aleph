4efdf1dc92a92027e6a751a1081094df0cf31f7d
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-09-21 18:27:09 +0530

    ONE OR MANY BABY!!! Thank the god in heaven that I didn't need to write my own deserializer serde is way too cool.

--- src/manifest/mod.rs
+++ src/manifest/mod.rs
@@ -12,19 +12,19 @@ pub struct Manifest {
     // NOTE that most of the Vec<T> are likely to fail
     // will require the followign implementation
     // https://serde.rs/string-or-struct.html
-    pub bin: Option<Vec<String>>,
-    pub depends: Option<Vec<String>>,
+    pub bin: Option<OneOrMany<String>>,
+    pub depends: Option<OneOrMany<String>>,
     pub env_add_path: Option<String>,
     // I am not sure if this works the way I hope it works
     // TODO: Test whether this works in accordance to the manifest
-    pub env_set: Option<Vec<(String, String)>>,
+    pub env_set: Option<OneOrMany<(String, String)>>,
     pub extract_dir: Option<String>,
     pub extract_to: Option<String>,
-    pub hash: Option<Vec<String>>,
+    pub hash: Option<OneOrMany<String>>,
     pub innosetup: Option<bool>,
-    pub notes: Option<Vec<String>>,
+    pub notes: Option<OneOrMany<String>>,
     pub psmodule: Option<ModuleName>,
-    pub url: Option<Vec<String>>,
+    pub url: Option<OneOrMany<String>>,
 
     // incomplete implementation
     // in the scoop manifest comments start with "##" we'll have to make a custom deserializer for
@@ -50,4 +50,11 @@ pub struct Manifest {
 #[derive(Serialize, Deserialize)]
 pub struct ModuleName {
     pub name: String,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[serde(untagged)]
+pub enum OneOrMany<T> {
+    One(T),
+    Many(Vec<T>)
 }
--- tests/manifest_test.rs
+++ tests/manifest_test.rs
@@ -11,3 +11,18 @@ fn required_attributes() {
     assert_eq!("https://some_homepage.com", data.homepage);
     assert_eq!("unlicensed", data.license);
 }
+
+#[test]
+fn str_or_struct_attributes() {
+    use aleph::manifest::Manifest;
+    use aleph::manifest::OneOrMany as OM;
+
+    let data = std::fs::read_to_string("./tests/sample_data/str_or_struct_attrs.json")
+        .expect("Failed to retreive sample data");
+    let data: Manifest = serde_json::from_str(&data)
+        .expect("Failed to parse data\n");
+
+  	assert_eq!(OM::One("https://github.com/lukesampson/cowsay-psh/archive/master.zip".to_owned()), data.url.unwrap());
+  	assert_eq!("cowsay-psh-master", data.extract_dir.unwrap());
+    assert_eq!(OM::Many(vec!["cowsay.ps1".to_string()]), data.bin.unwrap());
+}
