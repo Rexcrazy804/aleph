efce96a841368c1c4d59d1d9eb12e22265a558de
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2024-11-29 19:13:45 +0530

    improvements to the manifest install test + additional test manifest

--- tests/install_test.rs
+++ tests/install_test.rs
@@ -6,20 +6,39 @@ use aleph::{
 #[test]
 #[ignore]
 fn installation() {
-    //println!("{manifest:?}");
-    let home_dir = dbg!(get_home_directory()
-        .expect("Failed to get homeDIR")
-        .trim()
-        .to_owned());
+    use std::fs::read_to_string;
 
-    let manifest =
-        std::fs::read_to_string("./tests/sample_data/less.json").expect("Failed to read file");
-    let manifest: Manifest = serde_json::from_str(&manifest).expect("Failed to parse data");
+    const DEBUG_SKIP_COWSAY: bool = true;
+    const DEBUG_SKIP_LESS: bool = true;
+    const DEBUG_SKIP_FFMPEG: bool = false;
 
-    manifest_installer(&manifest, &home_dir);
+    const COWSAY_MANIFEST: &'static str = "./tests/sample_data/cowsay.json";
+    const LESS_MANIFEST: &'static str = "./tests/sample_data/less.json";
+    const FFMPEG_MANIFEST: &'static str = "./tests/sample_data/ffmpeg.json";
 
-    let manifest =
-        std::fs::read_to_string("./tests/sample_data/cowsay.json").expect("Failed to read file");
-    let manifest: Manifest = serde_json::from_str(&manifest).expect("Failed to parse data");
-    manifest_installer(&manifest, &home_dir);
+    let home_dir = get_home_directory().expect("Failed to get homeDIR");
+
+    if !DEBUG_SKIP_COWSAY {
+        let manifest = read_to_string(COWSAY_MANIFEST).expect("Failed to read file");
+        let manifest: Manifest = serde_json::from_str(&manifest).expect("Failed to parse data");
+        if let Err(error) = manifest_installer(&manifest, &home_dir) {
+            eprintln!("{error}")
+        }
+    }
+
+    if !DEBUG_SKIP_LESS {
+        let manifest = read_to_string(LESS_MANIFEST).expect("Failed to read file");
+        let manifest: Manifest = serde_json::from_str(&manifest).expect("Failed to parse data");
+        if let Err(error) = manifest_installer(&manifest, &home_dir) {
+            eprintln!("{error}")
+        }
+    }
+
+    if !DEBUG_SKIP_FFMPEG {
+        let manifest = read_to_string(FFMPEG_MANIFEST).expect("Failed to read file");
+        let manifest: Manifest = serde_json::from_str(&manifest).expect("Failed to parse data");
+        if let Err(error) = manifest_installer(&manifest, &home_dir) {
+            eprintln!("{error}")
+        }
+    }
 }
