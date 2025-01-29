d2c507249087e58d16050d1ce715b7b74ea2651c
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-01-29 06:58:54 +0530

    Completion of rewritten download_url function

--- src/powershell/utilities.rs
+++ src/powershell/utilities.rs
@@ -1,5 +1,6 @@
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::str::FromStr;
 
 // actually the only possible way for this to fail is for powershell to not be installed
 // in the operating system
@@ -15,47 +16,50 @@ pub fn get_home_directory() -> String {
 }
 
 /// attempts to download the given url to the provided directory and returns the path to the
-/// downloaded file. TODO correct the return type to be a a std::path::PATH
+/// downloaded file.
+/// # Panics
+/// - Unable to convert the ``download_location`` to a string
+/// - Unable to convert the string containing the path to the downloaded file into a ``PathBuf``
+///
+/// # Errors
+/// - Failure to run powershell (*powershell is not installed*)
+/// - Failure to find *wget.exe* in PATH
+/// - Invalid url
 pub fn download_url(url: &str, download_location: &Path) -> Result<PathBuf, String> {
-    println!("Downloading file {url}");
+    println!("Downloading file {url}...");
 
     let Ok(output) = Command::new("pwsh")
-        .args(["-c", "wget", url, "-P", download_location.to_str().unwrap()])
+        .args([
+            "-c",
+            "wget",
+            "-nv",
+            url,
+            "-P",
+            download_location.to_str().unwrap(),
+        ])
         .output()
     else {
+        // TODO in the event that this fails try using powershell's invoke web request [previous
+        // method] to download wget and append it to path and retry the download. Sanoy you can
+        // give this a shot
         return Err("Failed to execute request".to_owned());
     };
 
-    if let Ok(stdout) = String::from_utf8(output.stdout) {
-        println!("{stdout}");
-        println!("NO STDOUT");
-    } else {
-        println!("NO STDOUT");
+    // dk why but wget writes to stderr by default .w.
+    if let Ok(stderr) = String::from_utf8(output.stderr) {
+        let Some((_url, path)) = stderr.split_once("->") else {
+            return Err("Failed to parse wget output".to_string());
+        };
+        let Some((path, _)) = path.trim().split_once(' ') else {
+            return Err("Failed to parse wget output".to_string());
+        };
+
+        let path = path.trim_matches('"');
+        println!("Succesfully Downloaded to: {path}");
+        return Ok(PathBuf::from_str(path).expect("Failed to convert string to path"));
     }
 
-    //match String::from_utf8(output.stderr) {
-    //    Ok(stderr) => {
-    //        if stderr.is_empty() {
-    //            println!("Download Sucessfull");
-    //            Ok(file_path)
-    //        } else {
-    //            Err(stderr)
-    //        }
-    //    }
-    //    Err(_) => Err("Failed to parse stderr".to_string()),
-    //}
-
-    Err("ehe".to_owned())
-}
-
-fn get_filename(url: &str) -> Option<String> {
-    let last_token = url.split('/').last()?;
-
-    if last_token.contains('.') {
-        Some(last_token.to_string())
-    } else {
-        None
-    }
+    Err("Wget generated no output / Powershell is not installed".to_owned())
 }
 
 // First we need to get this to be able to extract simple .msi file from Destination to target
