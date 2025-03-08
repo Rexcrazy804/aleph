e9ef8062cf20acb88283d7a4804efef927ece2d6
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-03-08 10:40:52 +0530

    clarify licensing for json in manifest_install

--- src/scoopd/manifest_install.rs
+++ src/scoopd/manifest_install.rs
@@ -248,6 +248,32 @@ pub fn dependency_install(config: &AlephConfig, dependency: &str) -> Result<(), 
 }
 
 // ugly workaround will think of something later
+/* LICENSE FOR JSON CODE FOR 7zip from scoop main bucket
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*/
+
 const SEVENZIP_MANIFEST: &str = r#"{
     "version": "24.09",
     "description": "A multi-format file archiver with high compression ratios",
